5ea365da9a640e6d422a4ba111d817c8cb43861c
Christopher Smith <chris.smith@opin.ca>
2023-01-07 16:26:55 -0500

    Adding Webpack, Encore, and TailwindCSS

---
+++ tests/ApplicationAvailabilityFunctionalTest.php
@@ -0,0 +1,24 @@
+// tests/ApplicationAvailabilityFunctionalTest.php
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApplicationAvailabilityFunctionalTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function urlProvider()
+    {
+        yield ['/'];
+        yield ['/lucky/number'];
+    }
+}
